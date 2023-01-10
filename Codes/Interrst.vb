50b9f89f86da2900bdf85a55c1191846e6a6bac7
kamonpan0462 <122332031+kamonpan0462@users.noreply.github.com>
2023-01-10 09:57:57 +0700

    Add files via upload

---
+++ Codes/Interrst.vb
@@ -0,0 +1,17 @@
+﻿Imports System
+Module Interrst
+    Public deposit, rate As Double
+    Sub Main()
+        Dim Year1, Year2 As Double
+        rate = 0.02
+        deposit = 10000
+
+        Year1 = deposit * (1 + rate)
+        REM Console.WriteLine("First yaer = {0}", Year1)
+        Year2 = Year1 * (1 + rate)
+        Console.WriteLine("Second yaer = {0:C}", Year2)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
